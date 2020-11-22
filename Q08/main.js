ace22aca91ca52b986baf17f6ee217b349ed185a
AbdLaswi <abed.laswii@gmail.com>
2020-11-22 04:42:29 +0200

    Q8 10 mins

--- Q08/main.js
+++ Q08/main.js
@@ -1,4 +1,4 @@
-console.log('Problem Solving Q: 8');
+console.log("Problem Solving Q: 8");
 
 /* Unique Item */
 
@@ -6,17 +6,22 @@ console.log('Problem Solving Q: 8');
 Write a function that returns the number of unique items in an array.
 */
 
-function uniqueItems() {
+function uniqueItems(arr) {
   // YOUR CODE HERE
+  arr.sort();
+  for (let i = 0; i < arr.length; i++)
+    if (arr[i] === arr[i + 1]) {
+      arr.splice(i + 1, 1);
+      i--;
+    }
+  return arr.length;
 }
 
-/* 
-Examples:
-uniqueItems([1, 1, 1, 1, 1]) // => 1
-uniqueItems([3, 3, 5, 3]) // => 2
-uniqueItems([5, 1, 1, 5, 1]) // => 2
-uniqueItems([1, 6, 7]) // => 3
-uniqueItems([1, 2, 3, 1, 2, 3]) // => 3
-uniqueItems([5, 1, 2, 2, 1, 5]) // => 3
-uniqueItems([5, 77, 1, 3]) // => 4
-*/
+// Examples:
+// uniqueItems([5, 77, 1, 3]); // => 4
+// uniqueItems([1, 6, 7]); // => 3
+// uniqueItems([1, 2, 3, 1, 2, 3]); // => 3
+// uniqueItems([5, 1, 2, 2, 1, 5]); // => 3
+// uniqueItems([3, 3, 5, 3]); // => 2
+// uniqueItems([5, 1, 1, 5, 1]); // => 2
+// uniqueItems([1, 1, 1, 1, 1]); // => 1
