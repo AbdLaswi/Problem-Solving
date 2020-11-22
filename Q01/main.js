9b08c30bbc16c6352efbe83b9ef5171308d3931c
AbdLaswi <abed.laswii@gmail.com>
2020-11-22 02:40:20 +0200

    delete console.log from Q1 and solve Q2 20 mins I was stupid

--- Q01/main.js
+++ Q01/main.js
@@ -6,10 +6,9 @@ console.log("Problem Solving Q: 1 ");
 Have the function LongestWord(sen) take the sen parameter being passed and return the largest word in the string. If there are two or more words that are the same length, return the first word from the string with that length.
 */
 function longestWord(sen) {
-  let str = " ";
   // YOUR CODE HERE
+  let str = " ";
   sen = sen.split(" ");
-  console.log(sen);
   for (let i = 0; i < sen.length; i++) {
     if (sen[i].length > str.length) {
       str = sen[i];
