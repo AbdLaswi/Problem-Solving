65d72b14da84e9915eabd9ef37a1ac6c43a7219c
AbdLaswi <abed.laswii@gmail.com>
2020-12-11 01:19:12 +0200

    15mins

--- Q17/main.js
+++ Q17/main.js
@@ -1,4 +1,4 @@
-console.log('Problem Solving Q:17 ');
+console.log("Problem Solving Q:17 ");
 
 /*  */
 
@@ -6,13 +6,23 @@ console.log('Problem Solving Q:17 ');
 Given an arbitrary input string, return the first non-repeating character. For strings with all repeats, return 'sorry'
 */
 
-function firstNonRepeatedCharacter() {
+function firstNonRepeatedCharacter(str) {
   // YOUR CODE HERE
+  str = str.split("");
+  let char = str[0];
+  for (let i = 0; i < str.length; i++) {
+    str.shift();
+    if (!str.shift().includes(char)) {
+      return char;
+    } else {
+      char = str[i];
+    }
+  }
 }
 
 /* 
 Examples:
-firstNonRepeatedCharacter('ABCDBIRDUP') // => 'A'
+firstNonRepeatedCharacter("ABCDBIRDUP"); // => 'A'
 firstNonRepeatedCharacter('ALABAMA') // => 'L'
 firstNonRepeatedCharacter('Uber for horses') // => 'U'
 */
