25d4a400943f6194e164ad2ae37b0539a0e82bf4
AbdLaswi <abed.laswii@gmail.com>
2020-12-11 01:56:05 +0200

    25mins

--- Q17/main.js
+++ Q17/main.js
@@ -10,18 +10,19 @@ function firstNonRepeatedCharacter(str) {
   // YOUR CODE HERE
   str = str.split("");
   let char = str[0];
-  for (let i = 0; i < str.length; i++) {
+  for (let i = 0; i < str.length; i) {
     str.shift();
     if (!str.includes(char)) {
       return char;
     } else {
+      str.push(char);
       char = str[i];
     }
   }
 }
-
 /* 
 Examples:
+firstNonRepeatedCharacter("AABBCCDDP"); // => 'P'
 firstNonRepeatedCharacter("ABCDBIRDUP"); // => 'A'
 firstNonRepeatedCharacter('ALABAMA') // => 'L'
 firstNonRepeatedCharacter('Uber for horses') // => 'U'
