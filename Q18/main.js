c3bd76e18acd68e8e6b53a1eb87d38217de5cad4
AbdLaswi <abed.laswii@gmail.com>
2020-12-11 02:10:52 +0200

    3 mins

--- Q18/main.js
+++ Q18/main.js
@@ -1,4 +1,4 @@
-console.log('Problem Solving Q:18 ');
+console.log("Problem Solving Q:18 ");
 
 /*  factorial*/
 
@@ -7,13 +7,18 @@ Given an integer, n, return n! (n factorial)
 5! = 5 * 4 * 3 * 2 * 1
 */
 
-function factorial() {
+function factorial(num) {
   // YOUR CODE HERE
+  let answer = 1;
+  for (let i = num; i >= 0; i--) {
+    answer = answer * i;
+  }
+  return answer;
 }
 
 /* 
 Examples:
 factorial(1) // => 1
 factorial(3) // => 6
-factorial(4) // => 24
+factorial(4); // => 24
 */
