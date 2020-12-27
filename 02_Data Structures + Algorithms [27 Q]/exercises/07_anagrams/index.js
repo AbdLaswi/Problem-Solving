241cb008db9ca551c5c4af13deec0d13fd92a1a6
AbdLaswi <abed.laswii@gmail.com>
2020-12-27 19:15:13 +0200

    Q7 7min

--- 02_Data Structures + Algorithms [27 Q]/exercises/07_anagrams/index.js
+++ 02_Data Structures + Algorithms [27 Q]/exercises/07_anagrams/index.js
@@ -1,5 +1,5 @@
 // --- Directions
-// Check to see if two provided strings are anagrams of eachother.
+// Check to see if two provided strings are anagrams of each other.
 // One string is an anagram of another if it uses the same characters
 // in the same quantity. Only consider characters, not spaces
 // or punctuation.  Consider capital letters to be the same as lower case
@@ -7,7 +7,29 @@
 //   anagrams('rail safety', 'fairy tales') --> True
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
-
-function anagrams(stringA, stringB) {}
+const removeSymbol = (str) => {
+  const symbol = /[.,!,@,#,$,%,^,&,*,?,_,~,-,(,)-, ]/g;
+  symbolArr = str.match(symbol);
+  str = str.split("").sort();
+  if (symbolArr !== null) {
+    for (j = 0; j < symbolArr.length; j++) {
+      for (let i = 0; i < str.length; i++) {
+        if (str[i] === symbolArr[j]) {
+          str.splice(i, 1);
+          i--;
+        }
+      }
+    }
+  }
+  return str;
+};
+function anagrams(stringA, stringB) {
+  if (stringA.length !== stringB.length) {
+    return false;
+  }
+  stringA = removeSymbol(stringA.toLowerCase());
+  stringB = removeSymbol(stringB.toLowerCase());
+  return stringB.join("") === stringA.join("");
+}
 
 module.exports = anagrams;
