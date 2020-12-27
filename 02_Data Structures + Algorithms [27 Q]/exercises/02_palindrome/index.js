a12ce356267a25cf244ba3362402ab7a9b688989
AbdLaswi <abed.laswii@gmail.com>
2020-12-27 15:20:38 +0200

    Q2 10min

--- 02_Data Structures + Algorithms [27 Q]/exercises/02_palindrome/index.js
+++ 02_Data Structures + Algorithms [27 Q]/exercises/02_palindrome/index.js
@@ -7,6 +7,9 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  str = str.split("");   
+  return str.join("") === str.reverse().join("");
+}
 
 module.exports = palindrome;
