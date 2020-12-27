272ef7d8cf4ca26c85531b40260d64ca682d5cef
AbdLaswi <abed.laswii@gmail.com>
2020-12-27 16:00:51 +0200

    Q4 7min

--- 02_Data Structures + Algorithms [27 Q]/exercises/04_maxchar/index.js
+++ 02_Data Structures + Algorithms [27 Q]/exercises/04_maxchar/index.js
@@ -5,6 +5,25 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  str = str.split("");
+  let [freq, count, char] = [0, 0, null];
+  if (str.length === 1) {
+    char = str[0];
+  }
+  for (let i = 0; i < str.length; i++) {
+    if (count > freq) {
+      freq = count;
+      char = str[i - 1];
+    }
+    count = 0;
+    for (let j = 0; j < str.length; j++) {
+      if (str[i] === str[j]) {
+        count += 1;
+      }
+    }
+  }
+  return char;
+}
 
 module.exports = maxChar;
