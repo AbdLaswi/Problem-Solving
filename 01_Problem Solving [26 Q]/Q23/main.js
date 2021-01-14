d563b5660bba1484aaf8c2be53eca94119002368
AbdLaswi <abed.laswii@gmail.com>
2021-01-14 23:18:22 +0200

    Q23

--- 01_Problem Solving [26 Q]/Q23/main.js
+++ 01_Problem Solving [26 Q]/Q23/main.js
@@ -9,6 +9,10 @@ If the url does not contain a query string, return undefined
 
 function parseQueryString() {
   // YOUR CODE HERE
+  /* 
+  i have an idea how to deal with this question but it needs more time, and I dont have this time right now (the deadline) 
+  but I will solve it anyway 
+  */
 }
 
 
